f4ab3cea2728abd2564f6645a8cd8788932dc443
Vidhi Doshi <32307080+VidhiDoshi123@users.noreply.github.com>
2022-03-23 16:59:01 +0530

    Time: 61 ms (55.54%), Space: 14.5 MB (18.17%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        def bse_array(array,low,high,target):
+            if(low<=high):
+                mid = low + (high - low) // 2
+                if(array[mid] == target):
+                    return True
+                elif(target <array[mid]):
+                    return bse_array(array,low,mid-1,target)
+                elif(target > array[mid]):
+                    return bse_array(array,mid+1,high,target)
+            return False
+        for i in range(0,len(matrix)):
+            row = matrix[i]
+            result =bse_array(row,0,len(row)-1,target)
+            if(result):
+                return True
+        return False
