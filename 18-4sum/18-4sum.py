155319b5e1d3b0cfe51e9c1013326292c3b6a32e
Vidhi Doshi <32307080+VidhiDoshi123@users.noreply.github.com>
2022-07-14 17:37:55 +0530

    Time: 3769 ms (5.34%), Space: 14 MB (64.73%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,28 +1,18 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        ans = []
         nums.sort()
-        for i in range(len(nums)):
-            target_3 = target - nums[i]
-            for j in range(i+1,len(nums)-2):
-                if(j==0 or (j>0)):
-                    b = nums[j]
-                    low = j+1
-                    high = len(nums)-1
-                    target_2 = target_3 - b
-                    while(low<high):
-                        if(nums[low]+nums[high]==target_2):
-                            if([nums[i],b,nums[low],nums[high]] not in ans):
-                                ans.append([nums[i],b,nums[low],nums[high]])
-                            while(low<high and nums[low]==nums[low+1]):
-                                low+=1
-                            while(low<high and nums[high]==nums[high-1]):
-                                high-=1
-                            low +=1
-                            high-=1
-                        elif(nums[low]+nums[high]<target_2):
-                            low+=1
-                        else:
-                            high-=1
+        ans=set()
+        for a in range(len(nums)):
+            for b in range(a+1,len(nums)):
+                left=b+1
+                right=len(nums)-1
+                while(left<right):
+                    if(nums[a]+nums[b]+nums[left]+nums[right]==target):
+                        ans.add((nums[a],nums[b],nums[left],nums[right]))
+                        left+=1
+                        right-=1
+                    elif(nums[a]+nums[b]+nums[left]+nums[right]<target):
+                        left+=1
+                    else:
+                        right-=1
         return ans
-                    
