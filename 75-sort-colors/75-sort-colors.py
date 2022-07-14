50441e5a89f3141f39ed1dff5d0d25389e41035b
Vidhi Doshi <32307080+VidhiDoshi123@users.noreply.github.com>
2022-07-14 17:03:33 +0530

    Time: 42 ms (75.83%), Space: 14 MB (15.16%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,13 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count0 = nums.count(0)
-        count1 = nums.count(1)
-        count2 = nums.count(2)
-        for i in range(0,count0):
-            nums[i] = 0
-        for i in range(count0,count0+count1):
-            nums[i] = 1
-        for i in range(count0+count1,count0+count1+count2):
-            nums[i] = 2
+        i,j,k=0,0,len(nums)-1
+        while(j<=k):
+            if(nums[j]==0):
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+                j+=1
+            elif(nums[j]==1):
+                j+=1
+            else:
+                nums[j],nums[k]=nums[k],nums[j]
+                k-=1
         return nums
